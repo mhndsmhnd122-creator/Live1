d2e5551b57002720ffb5872e52571121e2fe163f
mhndsmhnd122-creator <mhndsmhnd122@gmail.com>
2026-08-19 23:04:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import urllib.request
 
 # يوزر القناة على كيك
-KICK_USERNAME = "Peerless"
+KICK_USERNAME = "TAF86"
 
 # مفتاح Restream ورابط الخادم
 RESTREAM_KEY = "re_11725544_event1f24e3174647428d86fc1329252bbf36"
